e42c7bd9cd8ceeba383bc8e4362c3f961a7ce882
bbarr <brendanbarr.web@gmail.com>
2011-08-12 12:52:58 -0400

    rid of view helpers

--- lib/tba/apps/base.rb
+++ lib/tba/apps/base.rb
@@ -4,8 +4,7 @@ class TBA::Base < Sinatra::Base
 
   set :static, true
   set :public, File.join(File.dirname(__FILE__), '../../public/')
-  
-  helpers ViewHelpers
+
   helpers SessionHelpers
   
 end
